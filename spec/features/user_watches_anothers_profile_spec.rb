253d226d1971f7d2d6562fa98eb44652129d12a1
cnearing <katerina4227@gmail.com>
2022-09-29 19:58:57 +0300

    edit feature user_watches_anothers_profile

--- spec/features/user_watches_anothers_profile_spec.rb
+++ spec/features/user_watches_anothers_profile_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature 'USER creates a game', type: :feature do
+RSpec.feature "USER watches another's profile", type: :feature do
   let(:user) { FactoryGirl.create :user }
+  let(:another_user) { FactoryGirl.create :user }
 
   let!(:games) do
-    [ FactoryGirl.create(:game, user: user, created_at: Time.now, current_level: 1),
-      FactoryGirl.create(:game, user: user, created_at: '2022-06-26 19:07:00', finished_at: '2022-06-26 19:08:08',
+    [ FactoryGirl.create(:game, user: another_user, created_at: Time.now, current_level: 1),
+      FactoryGirl.create(:game, user: another_user, created_at: '2022-06-26 19:07:00', finished_at: '2022-06-26 19:08:08',
         current_level: 5, prize: 12345) ]
   end
 
+  before { login_as user }
+
   scenario 'successfully' do
-    visit '/users/2'
+    visit "/users/#{another_user.id}"
 
     expect(page).not_to have_content 'Сменить имя и пароль'
 
